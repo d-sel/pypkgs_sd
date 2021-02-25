cf3e19cc7a4851938c55bc7637740731ac257711
Selma Duric <selma@duric.ca>
2021-02-24 21:55:29 -0800

    Added unit tests for catbind

--- tests/test_pypkgs_sd.py
+++ tests/test_pypkgs_sd.py
@@ -1,5 +1,14 @@
 from pypkgs_sd import __version__
 from pypkgs_sd import pypkgs_sd
+import pandas as pd
+
 
 def test_version():
-    assert __version__ == version ['0.1.0']: 
+    assert __version__ == '0.1.0'
+
+def test_catbind():
+    a = pd.Categorical(["character", "hits", "your", "eyeballs"])
+    b = pd.Categorical(["but", "integer", "where it", "counts"])
+    assert ((pypkgs_sd.catbind(a, b)).codes == [1, 4, 7, 3, 0, 5, 6, 2]).all()
+    assert ((pypkgs_sd.catbind(a, b)).categories == ["but", "character",
+            "counts", "eyeballs", "hits", "integer", "where it", "your"]).all()
